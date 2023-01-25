4f8ad63ee4b8e58fdb58fe23c857a30f3713117e
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-25 21:42:20 +0700

    initialize project

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const compression = require("compression");
+const helmet = require("helmet");
+require("dotenv").config();
+
+//initialize express
+const app = express();
+
+// use packages
+app.use(helmet());
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// server listening
+const PORT = process.env.PORT || 7022;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
