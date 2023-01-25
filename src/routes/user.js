e5d5319c7c24d9749501f8890d046fd425a83973
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-26 00:57:28 +0700

    add login route

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controllers = require("../controllers/index");
 
 router.post("/register", controllers.user.register);
+router.post("/login", controllers.user.login);
 
 module.exports = router;
