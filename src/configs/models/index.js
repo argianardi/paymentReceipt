6c9257c1dedc2306228369d3016368c8bc264e99
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-26 03:13:17 +0700

    add barang model

--- src/configs/models/index.js
+++ src/configs/models/index.js
@@ -1,8 +1,10 @@
 const models = {};
 const user = require("./user");
 const perusahaan = require("./perusahaan");
+const barang = require("./barang");
 
 models.user = user;
 models.perusahaan = perusahaan;
+models.barang = barang;
 
 module.exports = models;
