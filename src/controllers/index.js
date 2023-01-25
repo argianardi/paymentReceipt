a9415fda50990f1a52ce025bad2b177a0d96f177
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-26 01:48:43 +0700

    add perusahaan controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,8 @@
 const controllers = {};
 const user = require("./user");
+const perusahaan = require("./perusahaan");
 
 controllers.user = user;
+controllers.perusahaan = perusahaan;
 
 module.exports = controllers;
