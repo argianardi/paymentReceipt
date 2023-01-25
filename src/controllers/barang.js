190ca51fb7d4c9a63162c8bb4af4f4461601c33e
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-26 03:18:28 +0700

    add get all data request

--- src/controllers/barang.js
+++ src/controllers/barang.js
@@ -27,4 +27,26 @@ controllerBarangs.post = async (req, res) => {
   }
 };
 
+// get all data
+controllerBarangs.getAll = async (req, res) => {
+  try {
+    const barangs = await models.barang.findAll();
+    if (barangs.length > 0) {
+      res.status(200).json({
+        message: "Semua barang berhasil ditemukan",
+        data: barangs,
+      });
+    } else {
+      res.status(404).json({
+        message: "Data barang tidak ditemukan",
+        data: [],
+      });
+    }
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+
 module.exports = controllerBarangs;
--- src/routes/barang.js
+++ src/routes/barang.js
@@ -3,5 +3,6 @@ const router = express.Router(); //include express router
 const controllers = require("../controllers/index"); //import projects controllers
 
 router.post("/barang", controllers.barang.post);
+router.get("/barang", controllers.barang.getAll);
 
 module.exports = router;
