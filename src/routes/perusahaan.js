3edd73139012b7d7c607a58e88b5884b24c17a87
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-26 02:43:32 +0700

    add get all data perusahaan router

--- src/routes/perusahaan.js
+++ src/routes/perusahaan.js
@@ -3,5 +3,6 @@ const router = express.Router(); //include express router
 const controllers = require("../controllers/index"); //import projects controllers
 
 router.post("/perusahaan", controllers.perusahaan.post);
+router.get("/perusahaan", controllers.perusahaan.getAll);
 
 module.exports = router;
