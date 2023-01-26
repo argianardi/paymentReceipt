4e3dd05f9d1d7958ddf7e40ca0751f576bc5fea6
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-26 09:15:57 +0700

    add transaksi controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,9 +2,11 @@ const controllers = {};
 const user = require("./user");
 const perusahaan = require("./perusahaan");
 const barang = require("./barang");
+const transaksi = require("./transaksi");
 
 controllers.user = user;
 controllers.perusahaan = perusahaan;
 controllers.barang = barang;
+controllers.transaksi = transaksi;
 
 module.exports = controllers;
